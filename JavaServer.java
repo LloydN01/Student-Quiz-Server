ddd5ea4a12b402cc78ac73c3f41a00164b730327
LloydN01 <lloydna0527@gmail.com>
2023-05-01 14:37:16 +0800

    server can talk to client for infinite loops

--- JavaServer.java
+++ JavaServer.java
@@ -6,7 +6,6 @@ public class JavaServer {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = null;
         boolean listening = true;
-        Scanner scanner = new Scanner(System.in);
 
         try {
             serverSocket = new ServerSocket(9999); // set port number
@@ -24,23 +23,50 @@ public class JavaServer {
             Socket clientSocket = serverSocket.accept();
             System.out.println("Client connected: " + clientSocket.getInetAddress().getHostName());
 
-            // Send initial welcome message to client
-            PrintWriter writer = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()), true);
-            writer.println("Welcome to the server!"); // println() writes a string, followed by the '\n' character, to the output stream
-            writer.flush(); // flush() flushes the output stream and forces any buffered output bytes to be written out
-            
-            // Read from client
-            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            String receivedLine = reader.readLine(); // readLine() reads a line of text until it encounters a '\n' or '\r' character
-            System.out.println("Received message from client: " + receivedLine);
-
-            // Send customer message to client
-            System.out.print("Enter message to send to client: ");
-            String sendLine = scanner.nextLine();
-            writer.println(sendLine);
-            writer.flush();
+            // Create a new thread for the client
+            ClientThread clientThread = new ClientThread(clientSocket);
+            clientThread.start();
         }
         serverSocket.close();
-        scanner.close();
+    }
+}
+
+class ClientThread extends Thread {
+    private Socket clientSocket = null;
+
+    public ClientThread(Socket socket) {
+        super("ClientThread");
+        clientSocket = socket;
+    }
+
+    public void run() {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            // Send initial welcome message to client
+            writer.println("Welcome to the server!"); // println() writes a string, followed by the '\n' character, to the output stream
+            writer.flush(); // flush() flushes the output stream and forces any buffered output bytes to be written out
+
+            while(clientSocket.isConnected()){
+                // Read from client
+                String receivedLine = reader.readLine(); // readLine() reads a line of text until it encounters a '\n' or '\r' character
+                System.out.println("Received message from client: " + receivedLine);
+
+                // Send customer message to client
+                System.out.print("Enter message to send to client: ");
+                String sendLine = scanner.nextLine();
+                writer.println(sendLine);
+                writer.flush();
+            }
+
+            writer.close();
+            reader.close();
+            clientSocket.close();
+            scanner.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
