57a082402dbe8a9c0427c741e14d327a7645d2cb
LloydN01 <lloydna0527@gmail.com>
2023-05-02 18:11:33 +0800

    placeholder answer and assuming java questions

--- PythonClient.py
+++ PythonClient.py
@@ -2,37 +2,43 @@ import socket
 import select
 
 HOST = '10.135.156.144'  # set the host
-PORT = 9999
-PORT2 = 9998
+javaPort = 9999
+pythonPort = 9998
 
-# with (socket.socket(socket.AF_INET, socket.SOCK_STREAM),socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as (s,t):
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+javaQB = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+pythonQB = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server.connect((HOST, PORT))
-server2.connect((HOST, PORT2))
+javaQB.connect((HOST, javaPort))
+pythonQB.connect((HOST, pythonPort))
 
-server.setblocking(False)
-server2.setblocking(False)
+javaQB.setblocking(False)
+pythonQB.setblocking(False)
  
-inputs = [server, server2]
+inputs = [javaQB, pythonQB]
 outputs = []
 
 while inputs:
     readable, writable, exceptional = select.select(inputs, outputs, inputs)
-    # print(readable)
-    # for i in writable:
-    #     data = "hello my brother"
-    #     sent = i.sendall(data.encode())
 
     for recievedData in readable:
-        recievedMsg = recievedData.recv(1024)
-        if recievedMsg:
-            print('Received from', recievedData.getpeername()[1], ':', recievedMsg.decode())
+        recievedQuestion = recievedData.recv(1024)
+        if recievedQuestion:
+            print('Received from', recievedData.getpeername()[1], ':', recievedQuestion.decode())
+
+    # Handle exceptional sockets
+    for error in exceptional:
+        print('Exceptional condition on', error.getpeername())
+        inputs.remove(error)
+        if error in outputs:
+            outputs.remove(error)
+        error.close()
     
-    # message = input("Enter a message to send: ") + "\n" # add a new line character to the end of the message
-    # t.sendall(message.encode())
-    server.send("//JAVA//hello my brother\n".encode())
-    server2.send("//PYTHON//hello my sister\n".encode())
-server.close()
-server2.close()
+    answer = "Hello, World!" + "\n" # TODO: This variable will need to be updated when the user submits an answer
+    isJava = True # TODO: Need a way to keep track of this variable and change it according to the question
+    if(isJava):
+        javaQB.sendall(answer.encode())
+    else:
+        pythonQB.sendall(answer.encode())
+
+javaQB.close()
+pythonQB.close()
